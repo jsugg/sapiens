72e6024d109dc3937a07bb8f1a83bbdd1ccb602b
Juan Sugg <juanpedrosugg@gmail.com>
2023-03-09 09:10:44 -0300

    little cat hand

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 var sapiens = document.getElementById("sapiens");
 
 sapiens.onmouseover = function() {
-  sapiens.style.fontSize = "120%";
+  sapiens.style.fontSize = "400%";
   sapiens.style.backgroundColor = "lightgrey";
   sapiens.style.color = "black";
-  document.getElementById("tagline").style.color = "black";
+  document.getElementById("main").style.transform = "translateY(-50%)";
+  document.getElementById("main").style.transform = "translateX(-50%)";
+  document.getElementById("main").style.transition = "all 0.5s";
+  document.getElementById("tagline").style.transition = "all 0.5s";
+  document.getElementById("tagline").style.color = "white";
 }
 
 sapiens.onmouseleave = function() {
   sapiens.style.fontSize = "3em";
   sapiens.style.backgroundColor = "black";
   sapiens.style.color = "white";
-  document.getElementById("tagline").style.color = "white";
+  document.getElementById("main").style.transform = "translateY(-50%)";
+  document.getElementById("main").style.transform = "translateX(-50%)";
+  document.getElementById("main").style.transition = "all 0.5s";
+  document.getElementById("tagline").style.transition = "all 0.5s";
+  document.getElementById("tagline").style.color = "darkgray";
 }
